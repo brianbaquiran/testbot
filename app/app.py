ae58d708b965c1341e949d713f239b49705f88bc
Brian Baquiran <brianbaquiran@gmail.com>
2016-10-07 21:08:37 +0800

    Add initial app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    if request.method == 'GET':
+        return 'Hello World!'
+    if request.method == 'POST':
+        print('A value for debugging')
+        return 'You POSTED!'
 
-@app.route('/webhook')
-def handle_():
-    return
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method == 'GET':
+        # Facebook is validating the webhook subscription.
+        # See https://developers.facebook.com/docs/graph-api/webhooks
+        # Section: Handling Verification Requests
+        hub_challenge = request.args.get('hub.challenge','')
+        if hub_challenge == '':
+            return "Hello, you didn't specify a hub.challenge"
+        return hub_challenge
+    elif request.method == 'POST':
+        return ""
